c681e9c97fc72a50ad29ae67c771e38b44f05e4d
Fabio Simao <fabio.simao@bjss.com>
2018-02-26 11:26:37 +0000

    Adds url root to the post on the search page (to make it work on aws) Fixes regex that validates penalty reference

--- src/server/validation/penaltyReference.js
+++ src/server/validation/penaltyReference.js
@@ -2,5 +2,5 @@ import { check } from 'express-validator/check';
 
 export default [
   check('penalty_ref').isLength({ min: 8, max: 18 }),
-  check('penalty_ref').matches(/^[a-zA-Z0-9_-]+$/g),
+  check('penalty_ref').matches(/^[a-zA-Z0-9_-]+$/),
 ];
