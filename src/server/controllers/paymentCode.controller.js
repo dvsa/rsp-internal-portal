21b458e8045b3eceaeec7b3e80ca9fb9c3b4f0f4
Fabio Simao <fabio.simao@bjss.com>
2018-03-12 14:31:25 +0000

    Adds page for payment by cheque Adds few style updates to tables Refactors and renames paymentCodeController (from paymentController) Adds (new) paymentController

---
+++ src/server/controllers/paymentCode.controller.js
@@ -0,0 +1,41 @@
+import { validationResult } from 'express-validator/check';
+import paymentCodeValidation from './../validation/paymentCode';
+import PenaltyService from './../services/penalty.service';
+import config from './../config';
+
+const penaltyService = new PenaltyService(config.penaltyServiceUrl);
+
+// Removes all non-alphanumeric characters and converts to lowercase
+export const normalizePaymentcode = (req, res, next) => {
+  if (req.body.payment_code) {
+    req.body.payment_code = req.body.payment_code.replace(/\W|_/g, '').toLowerCase();
+  }
+  next();
+};
+
+export const validatePaymentCode = [
+  normalizePaymentcode,
+  paymentCodeValidation,
+  (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      res.redirect('../?invalidPaymentCode');
+    } else {
+      next();
+    }
+  },
+];
+
+export const getPenaltyDetails = [
+  paymentCodeValidation,
+  (req, res) => {
+    const paymentCode = req.params.payment_code;
+
+    penaltyService.getByPaymentCode(paymentCode).then((details) => {
+      res.render('penalty/penaltyDetails', details);
+    }).catch(() => {
+      res.redirect('../?invalidPaymentCode');
+    });
+  },
+];
