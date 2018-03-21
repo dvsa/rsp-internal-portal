b8a4861adf8d092ffc816fe78854cec219bc9458
Fabio Simao <fabio.simao@bjss.com>
2018-03-21 15:22:27 +0000

    Adds user authentication functionality (using authorisation code grant) Adds middleware to protect routes from anonymous access Adds minor improvements to the httpclient

--- src/server/controllers/main.controller.js
+++ src/server/controllers/main.controller.js
@@ -1,3 +1,8 @@
+import AuthService from '../services/auth.service';
+import config from '../config';
+
+const authService = new AuthService(config.cognitoUrl);
+
 // Robots
 export const robots = (req, res) => {
   res.type('text/plain');
@@ -28,4 +33,38 @@ export const searchPenalty = (req, res) => {
   } else {
     res.render('main/index', { invalidRequest: true });
   }
+};
+
+export const authenticate = (req, res) => {
+  const {
+    identityProvider,
+    redirectUri,
+    clientId,
+    clientSecret,
+  } = config;
+
+  const responseType = 'code';
+
+  res.redirect(`${config.cognitoUrl}/oauth2/authorize?identity_provider=${identityProvider}&redirect_uri=${encodeURIComponent(redirectUri)}&response_type=${responseType}&client_id=${clientId}&client_secret=${clientSecret}`);
+};
+
+export const login = (req, res) => {
+  if (req.query.code) {
+    authService.requestAccessToken(req.query.code).then((token) => {
+      res.cookie('rsp_access', { accessToken: token.access_token, idToken: token.id_token }, { maxAge: token.expires_in * 1000, httpOnly: true });
+      res.cookie('rsp_refresh', { refreshToken: token.refresh_token }, { maxAge: 2592000000, httpOnly: true });
+      res.redirect('/');
+    }).catch(() => {
+      // Failed to get an access token - Get a new authorization code and try again
+      authenticate(req, res);
+    });
+  } else {
+    authenticate(req, res);
+  }
+};
+
+export const logout = (req, res) => {
+  res.clearCookie('rsp_access');
+  res.clearCookie('rsp_refresh');
+  res.redirect(`${config.cognitoUrl}/logout?client_id=${config.clientId}&logout_uri=${encodeURIComponent(config.redirectUri)}`);
 };
---
+++ src/server/services/auth.service.js
@@ -0,0 +1,52 @@
+import { encode } from 'base-64';
+import queryString from 'querystring';
+import createHttpClient from '../utils/httpclient';
+import config from '../config';
+
+export default class AuthService {
+  constructor(serviceUrl) {
+    const secret = encode(`${config.clientId}:${config.clientSecret}`);
+
+    const defaults = {
+      headers: {
+        Authorisation: `Basic ${secret}`,
+      },
+    };
+
+    this.httpClient = createHttpClient(serviceUrl, defaults);
+  }
+
+  requestAccessToken(code) {
+    const payload = {
+      grant_type: 'authorization_code',
+      client_id: config.clientId,
+      client_secret: config.clientSecret,
+      redirect_uri: config.redirectUri,
+      code,
+    };
+
+    const promise = new Promise((resolve) => {
+      this.httpClient.post('/oauth2/token', queryString.stringify(payload)).then((result) => {
+        resolve(result.data);
+      });
+    });
+    return promise;
+  }
+
+  refreshAccessToken(refreshToken) {
+    const payload = {
+      grant_type: 'refresh_token',
+      client_id: config.clientId,
+      client_secret: config.clientSecret,
+      refresh_token: refreshToken,
+    };
+
+    const promise = new Promise((resolve, reject) => {
+      this.httpClient.post('/oauth2/token', queryString.stringify(payload)).then((result) => {
+        resolve(result.data);
+      }).catch(error => reject(error));
+    });
+
+    return promise;
+  }
+}
