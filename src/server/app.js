0136bfff9204a04ec4e3cf1e6f0e471668e74eff
Fabio Simao <fabio.simao@bjss.com>
2018-03-22 09:36:11 +0000

    Adds user information to the app locals so it's available to all routes (and views) Adds logged in user name to the header

--- src/server/app.js
+++ src/server/app.js
@@ -71,10 +71,6 @@ app.use((req, res, next) => {
 });
 
 app.use(cookieParser());
-<<<<<<< HEAD
-=======
-
->>>>>>> Adds user authentication functionality (using authorisation code grant)
 app.use(awsServerlessExpressMiddleware.eventContext());
 app.use('/', routes);
 
