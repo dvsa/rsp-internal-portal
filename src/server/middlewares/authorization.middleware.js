228a919bdd30556c6413c73e5194cbef0cfb7867
Fabio Simao <fabio.simao@bjss.com>
2018-03-28 13:52:45 +0100

    Adds role based authorization, User must have an authorised role ('admin' or 'user') in order to access the system

--- src/server/middlewares/authorization.middleware.js
+++ src/server/middlewares/authorization.middleware.js
@@ -4,7 +4,7 @@ import config from '../config';
 import AuthService from '../services/auth.service';
 
 const authService = new AuthService(config.cognitoUrl);
-
+const authorizedRoles = ['user', 'admin'];
 const cognitoExpress = new CognitoExpress({
   region: config.region,
   cognitoUserPoolId: config.userPoolId,
@@ -47,11 +47,18 @@ export default (req, res, next) => {
       } else {
         // Get user information from the ID token
         const userInfo = jwtDecode(req.cookies.rsp_access.idToken);
-
-        // Ensure that user information is available through the application (including views)
-        req.app.set('rsp_user', userInfo);
-
-        next();
+        if (userInfo['custom:Role']) {
+          if (authorizedRoles.some(item => item === userInfo['custom:Role'].toLowerCase())) {
+            // Ensure that user information is available through the application (including views)
+            req.app.set('rsp_user', userInfo);
+          } else {
+            // User doesn't have an authorized role, forbid access
+            return res.sendStatus(403);
+          }
+          return next();
+        }
+        // User doesn't have any role, forbid access
+        return res.sendStatus(403);
       }
     });
   }
