63e59242352b6a3cfb1d6ac0de9123c16c7f6afb
Ammar Haider <ammar.haider@bjss.com>
2018-10-05 11:42:56 +0100

    uses new reverseGroupPayment method

--- src/server/controllers/payment.controller.js
+++ src/server/controllers/payment.controller.js
@@ -552,7 +552,7 @@ export const reverseGroupPayment = async (req, res) => {
       case 'CARD':
         cpmsService.reverseCardPayment(PaymentRef, penaltyType, paymentCode)
           .then(() => {
-            paymentService.reversePayment(paymentCode).then((response) => {
+            paymentService.reverseGroupPayment(paymentCode).then((response) => {
               logger.info(response);
               return res.redirect(`${config.urlRoot()}/payment-code/${paymentCode}`);
             }).catch((error) => {
@@ -567,7 +567,7 @@ export const reverseGroupPayment = async (req, res) => {
       case 'CHEQUE':
         cpmsService.reverseChequePayment(PaymentRef, penaltyType, paymentCode)
           .then(() => {
-            paymentService.reversePayment(paymentCode).then((response) => {
+            paymentService.reverseGroupPayment(paymentCode).then((response) => {
               logger.info(response);
               return res.redirect(`${config.urlRoot()}/payment-code/${paymentCode}`);
             }).catch((error) => {
@@ -582,7 +582,7 @@ export const reverseGroupPayment = async (req, res) => {
       // Postal orders and cash reversals are not handled by CPMS
       case 'POSTAL':
       case 'CASH':
-        paymentService.reversePayment(paymentCode).then((response) => {
+        paymentService.reverseGroupPayment(paymentCode).then((response) => {
           logger.info(response);
           return res.redirect(`${config.urlRoot()}/payment-code/${paymentCode}`);
         }).catch((error) => {
