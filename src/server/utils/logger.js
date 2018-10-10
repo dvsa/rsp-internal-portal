767b2111e46c79d27702e41fa82845ad460635ff
Ammar Haider <ammar.haider@bjss.com>
2018-10-10 17:45:14 +0100

    disables pretty printing of json output from errors

--- src/server/utils/logger.js
+++ src/server/utils/logger.js
@@ -2,7 +2,7 @@ import { createLogger, transports, format } from 'winston';
 import config from '../config';
 
 export default createLogger({
-  format: format.prettyPrint(),
+  format,
   json: true,
   transports: [
     // Console logs are shown on CloudWatch so this should be enough for time being
