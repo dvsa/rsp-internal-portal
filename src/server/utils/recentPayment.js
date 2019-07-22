836c9f729c9192582065f4a422b0183bd5784847
James Highfield <James.Highfield@bjss.com>
2019-07-22 10:33:33 +0100

    Fix lint

--- src/server/utils/recentPayment.js
+++ src/server/utils/recentPayment.js
@@ -4,7 +4,9 @@ import config from '../config';
 
 export const recentPayment = (timeSeconds) => {
   if (timeSeconds) {
-    const age = moment.duration(moment(moment.now()).diff(moment(timeSeconds * 1000))).asMilliseconds();
+    const now = moment(moment.now());
+    const paymentTime = moment(timeSeconds * 1000);
+    const age = moment.duration(now.diff(paymentTime)).asMilliseconds();
     return age < config.orphanedPaymentCheckingTime();
   }
   return false;
