dec2dd0dae5f7175fdfe3e9463fe0105b8970bf7
Jake Turner <jake.turner@dvsa.gov.uk>
2023-01-30 15:17:33 +0000

    fix: allow certain payment types to be reversed on the same day (#145)
    * fix: changed reversible checking flow to allow cash and postal orders to be reversible on same day
    
    - When the reversible flow was introduced, there was no checking on the type of payments, despite the fact only card payments must wait until the next working day to reverse
    - To fix, an if check has been added to the reversible utility, which checks if the payment type is CNP, CARD or CHEQUE
    - paymentType is now passed in to the reversible function too to allow for checking
    - Updated view to inform user that card or cheque payments cannot be reversed on the same day
    
    * test: added unit tests to isReversible spec to check each payment type and if it can be reversed

--- src/server/utils/isReversible.js
+++ src/server/utils/isReversible.js
@@ -1,10 +1,13 @@
-export function isReversible(paymentStatus, paymentDate) {
+export function isReversible(paymentStatus, paymentDate, paymentType) {
   if (paymentStatus === 'PAID') {
-    if (paymentDate) {
-      const todaysDate = new Date().setHours(0, 0, 0, 0);
-      const formatPaymentDate = new Date(paymentDate * 1000).setHours(0, 0, 0, 0);
-      return todaysDate > formatPaymentDate;
+    if (paymentType === 'CNP' || paymentType === 'CARD' || paymentType === 'CHEQUE') {
+      if (paymentDate) {
+        const todaysDate = new Date().setHours(0, 0, 0, 0);
+        const formatPaymentDate = new Date(paymentDate * 1000).setHours(0, 0, 0, 0);
+        return todaysDate > formatPaymentDate;
+      }
     }
+    return true;
   }
 
   return false;
